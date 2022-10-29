10ce1cdff0eda523f265f40831f71a3c5d15146d
Joshua <65114205+joshuajames@users.noreply.github.com>
2022-10-29 19:00:39 +1300

    Update bio.js
    Don't follow

--- src/components/bio.js
+++ src/components/bio.js
@@ -42,7 +42,7 @@ const Bio = () => {
           {` `}
           {author?.twitter && (
             <a href={`https://twitter.com/${author?.twitter || ``}`}>
-              You should follow them on Twitter
+              Don't follow him on Twitter, he's super boring
             </a>
           )}
         </p>
